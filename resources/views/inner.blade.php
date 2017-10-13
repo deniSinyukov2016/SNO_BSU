8d39e2fee839daef1857e457e2110df78ae3441f
Denis Sinyukov <sinyukov.denis2015@yandex.ru>
2017-10-13 20:29:25 +0300

    Изменение вида внутренних страниц

--- resources/views/inner.blade.php
+++ resources/views/inner.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.inner-page')
 @section('title', 'Новости')
 @section('content')
-    <p>Новости</p>
+    @forelse($posts as $post)
+        <article class="blog-post" data-id="{{$post->id}}">
+            <header>
+                <h2 class="blog-post-title"><a href="">{{$post->title}}</a></h2>
+                <p class="blog-post-meta"><span>{{\Carbon\Carbon::parse($post->created_at)->format('d.m.Y')}}</span></p>
+                <p></p>
+            </header>
+            {{str_limit($post->content, 350)}}
+            <a href="news/{{$post->id}}">Подробнее →</a>
+        </article>
+    @empty
+        <p>Постов нет</p>
+    @endforelse
+    {{--Вывод постраничной навигации из view/vendor/pagination/default.blade.php--}}
+    {!! $posts->render()!!}
+
 @endsection
